aa93ccc6168b85b7d6355ef7e8c43b6a40881e15
dhakalsuman83 <dhakalsuman83@gmail.com>
2022-04-11 14:05:22 +1000

    started the memories app

---
+++ server/controllers/posts.js
@@ -0,0 +1,23 @@
+const postMessage = require("../models/postModel.js");
+
+module.exports.getPosts = async (req, res) => {
+  try {
+    const postMessages = await postMessage.find();
+    console.log(postMessages);
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+module.exports.createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new postMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const postRoutes = require("./routes/posts.js");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true })); //the image could be large so we have initialized the limit for the image
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://dhakalsuman:suman12345@cluster0.rnu3p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
